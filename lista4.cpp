b5a33de0ab11c9ed95587e12359f3a7d7033cdc1
gagatx07 <133365037+gagatx07@users.noreply.github.com>
2024-06-07 14:37:41 -0300

    Initial commit

---
+++ lista4.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	/*int num;
+	cout << "Digite um número de 1 a 7";
+	cin >> num;
+
+	switch (num)
+	{
+	case 1:
+		cout << "Domingo";
+		break;
+	case 2:
+		cout << "Segunda";
+		break;
+	case 3:
+		cout << "Terça";
+		break;
+	case 4:
+		cout << "Quarta";
+		break;
+	case 5:
+		cout << "Quinta";
+		break;
+	case 6:
+		cout << "Sexta";
+		break;
+	case 7:
+		cout << "Sabado";
+		break;
+	default:
+		cout << "Numero invalido";
+		break;
+	}*/
+
+	/*int num;
+	cout << "Informe um numero";
+	cin >> num;
+	int num2;
+	cout << "Informe outro numero";
+	cin >> num2;
+	char operacao;
+
+	int soma = num + num2;
+	int sub = num - num2;
+	int mult = num * num2;
+	int div = num / num2;
+
+	cout << "Informe a operação desejada (+, -, *, /): ";
+	cin >> operacao;
+
+	switch (operacao)
+	{
+	case '+':
+		cout << "Resultado" << soma;
+		break;
+	case '-':
+		cout << "Resultado" << sub;
+		break;
+	case '*':
+		cout << "Resultado" << mult;
+		break;
+	case '/':
+		cout << "Resultado" << div;
+		break;
+	default:
+		cout << "Numero invalido";
+		break;
+	}*/
+
+	/*cout << "Digite uma letra";
+
+	char letra;
+	cin >> letra;
+
+
+	switch (letra)
+	{
+	case 'A':
+		cout << "Vogal";
+		break;
+	case 'E':
+		cout << "Vogal";
+		break;
+	case 'I':
+		cout << "Vogal";
+		break;
+	case 'O':
+		cout << "Vogal";
+		break;
+	case 'U':
+		cout << "Vogal";
+		break;
+	case 'a':
+		cout << "Vogal";
+		break;
+	case 'e':
+		cout << "Vogal";
+		break;
+	case 'i':
+		cout << "Vogal";
+		break;
+	case 'o':
+		cout << "Vogal";
+		break;
+	case 'u':
+		cout << "Vogal";
+		break;
+	default:
+		cout << "Consoante";
+		break;
+	}*/
+
+}
